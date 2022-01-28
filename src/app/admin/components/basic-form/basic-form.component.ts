bd83c38873b5c55c9e97eaff03aae88f7ace342b
diego <you@example.com>
2022-01-28 02:59:35 -0500

    feat: manejo de binding de select y selects multiples

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -14,6 +14,9 @@ export class BasicFormComponent implements OnInit {
   dateField = new FormControl('#000000');
   ageField = new FormControl(12);
 
+  categoryField = new FormControl('category-2');
+  tagField = new FormControl('');
+
   constructor() { }
 
   ngOnInit(): void {
