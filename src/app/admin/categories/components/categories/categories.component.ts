6d341cb6a16c4561365244c56270eef28cb0445c
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-04-20 01:33:10 -0500

    feat: creando reto de lista de todas la categorias

--- src/app/admin/categories/components/categories/categories.component.ts
+++ src/app/admin/categories/components/categories/categories.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { CategoriesService } from '../../../../core/services/categories.service';
+import { Category } from 'src/app/core/models/category';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  public data$: Observable<Category[]>;
+
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllInfo();
   }
-
+  
+  getAllInfo(){
+    this.data$ = this.categoriesService.getAllCategories();
+  }
 }
