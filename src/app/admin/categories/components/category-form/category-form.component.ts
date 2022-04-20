023d46996a84d6005a06aad646a22a0f1bcb2c81
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-04-20 01:32:29 -0500

    creando servicio para categorias e interface de categoria

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  Validators, FormBuilder, FormGroup} from '@angular/forms';
+import { CategoriesService } from '../../../../core/services/categories.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private categoriesService: CategoriesService
+    )
+    { 
+      this.buildForm()
+    }
 
   ngOnInit(): void {
   }
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      image: ['', Validators.required]
+    });
+  }
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
+  get imageField() {
+    return this.form.get('image');
+  }
+
+  save(){
+    if (this.form.valid) {
+      this.CreateCategory();
+    }else{
+      this.form.markAllAsTouched
+    }
+  }
+
+  private CreateCategory(){
+    const data = this.form.value; 
+    this.categoriesService.createCategory(data)
+    .subscribe(res => {
+      console.log(res);
+      this.router.navigate(['./admin/categories']);
+    })
+  }
 }
