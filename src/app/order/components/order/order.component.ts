cbf2d974beec09bd0bb9ef911f972f4f425bfe9e
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-04-22 02:00:48 -0500

    feat: Create fields on demand, forms dinamicos con FormArray

--- src/app/order/components/order/order.component.ts
+++ src/app/order/components/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 import { Observable } from 'rxjs';
 
@@ -13,14 +14,46 @@ import { CartService } from './../../../core/services/cart.service';
 export class OrderComponent implements OnInit {
 
   products$: Observable<Product[]>;
+  form: FormGroup;
 
+  
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) {
     this.products$ = this.cartService.cart$;
+    this.buildForm();
   }
 
   ngOnInit() {
+    console.log(this.form.get('address'))
   }
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      address: this.formBuilder.array([])
+    });
+  }
+
+  addAddressField(){
+    this.addressField.push(this.createAddressField());
+  }
+
+  private createAddressField(): FormGroup{
+    return this.formBuilder.group({
+      zip: ['', Validators.required],
+      text: ['', Validators.required],
+    });
+  }
+
+  //getter
+  get addressField(): FormArray{
+    //return <FormArray>this.form.get('address')
+    return this.form.get('address')  as FormArray//return Abstract Control as FormArray
+  }
+
+  save(){
+    console.log(this.form.value)
+  }
 }
